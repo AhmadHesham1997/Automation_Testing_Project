b7a15bffb4e504d80df6c88e537efc3c1dd88795
AhmadHesham1997 <a.hesham2014@gmail.com>
2026-04-22 22:58:58 +0200

    Add registration page functionality with validation for empty username

--- src/main/java/Pages/P06_RegistrationPage.java
+++ src/main/java/Pages/P06_RegistrationPage.java
@@ -1,6 +1,10 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 //Test Case #4: Register with an empty username field
@@ -12,11 +16,73 @@ public class P06_RegistrationPage {
     WebDriverWait wait;
 
     //Locators
-
+    private final By signUpButton = By.xpath("//a[text()='حساب جديد']");
+    private final By usernameField = By.id("name");
+    private final By emailField = By.id("email");
+    private final By countryDropdown = By.xpath("(//select[@aria-hidden='true'])[1]");
+    private final By cityDropdown = By.xpath("(//select[@aria-hidden='true'])[2]");
+    private final By genderDropdown = By.xpath("(//select[@aria-hidden='true'])[3]");
+    private final By phoneNumberField = By.id("phone");
+    private final By passwordField = By.id("password");
+    private final By confirmPasswordField = By.id("confirm_password");
+    private final By checkBox = By.id("terms");
+   // private final By checkBox = By.xpath("//input[@type='checkbox']");
+    private final By createButton = By.xpath("//button[@type='submit']");
+    private final By validationErrorMessage = By.xpath("//p[contains(text(),'الاسم مطلوب')]");
 
     //constructor
     public P06_RegistrationPage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
     }
+
+    //Method to click on signupButton
+    public void clickSignUp() {
+        WebElement signUpBtn = wait.until(
+                ExpectedConditions.elementToBeClickable(signUpButton)
+        );
+        signUpBtn.click();
+        wait.until(ExpectedConditions.urlContains("/auth/register"));
+        //we didnt use driver.getwindowhandles because the signup page opens in the same tab it didnt switch to new tab,
+        // so we just wait until the URL contains /auth/register to ensure that we are on the signup page
+    }
+
+    //Entering data fields method using parameters
+    public void enterRegistrationData(String username, String email, String country, String city, String gender,  String phone, String password, String confirmPassword) {
+        driver.findElement(usernameField).sendKeys(username);
+
+        driver.findElement(emailField).sendKeys(email);
+
+        WebElement countryDropdownElement = driver.findElement(countryDropdown);
+        Select countrySelect = new Select(countryDropdownElement);
+        countrySelect.selectByVisibleText(country);
+
+        WebElement cityDropdownElement = driver.findElement(cityDropdown);
+        Select citySelect = new Select(cityDropdownElement);
+        citySelect.selectByVisibleText(city);
+
+        WebElement genderDropdownElement = driver.findElement(genderDropdown);
+        Select genderSelect = new Select(genderDropdownElement);
+        genderSelect.selectByVisibleText(gender);
+
+        driver.findElement(phoneNumberField).sendKeys(phone);
+
+        driver.findElement(passwordField).sendKeys(password);
+
+        driver.findElement(confirmPasswordField).sendKeys(confirmPassword);
+    }
+    //Clicking terms and create button
+    public void clickCreateButton() {
+        driver.findElement(checkBox).click();
+        WebElement createBtn = wait.until(
+                ExpectedConditions.elementToBeClickable(createButton)
+        );
+        createBtn.click();
+    }
+
+    //Assert that the validation error message is displayed when username field is empty
+    public boolean isValidationErrorMessageDisplayed() {
+        return driver.findElement(validationErrorMessage).isDisplayed();
+    }
+
 }
--- src/test/java/Test/RegestirationPageTest.java
+++ src/test/java/Test/RegestirationPageTest.java
@@ -1,4 +1,30 @@
 package Test;
+//Test Case #4: Register with an empty username field
+//•	Enter all data except the username field
+//•	Click “انشاء”
+//•	Assert validation error message are displayed “مطلوب المستخدم اسم”
+import Base.BaseTest;
+import Pages.P06_RegistrationPage;
+import org.testng.annotations.Test;
 
-public class RegestirationPageTest {
+public class RegestirationPageTest extends BaseTest {
+    P06_RegistrationPage registrationPage;
+
+    @Test
+    //Test Method
+    public void testRegistrationPage()
+    {    registrationPage = new P06_RegistrationPage(driver);
+        //click signup button to navigate to registration page
+        registrationPage.clickSignUp();
+        //Entering data fields except the username field
+        registrationPage.enterRegistrationData("", "test@example.com", "مصر", "القاهرة", "ذكر","123456789", "Password123*", "Password123*");
+        //click create button
+        registrationPage.clickCreateButton();
+
+        //Assert that the validation error message is displayed when username field is empty
+        boolean actual = registrationPage.isValidationErrorMessageDisplayed();
+        assert actual : "Validation error message is not displayed!";
+    }
+
+
 }
