86f2caf382778c351da898f25173c34b12cf1745
Ahmad Hesham <a.hesham2014@gmail.com>
2026-05-02 16:36:15 +0300

    Add Allure plugin configuration and enhance report generation scripts

--- src/test/java/TestNG_Listener/Listeners.java
+++ src/test/java/TestNG_Listener/Listeners.java
@@ -5,7 +5,7 @@ import org.testng.IExecutionListener;
 public class Listeners implements IExecutionListener {
     private final String DeleteJsoncommand = "cmd /c cd target/allure-results && del *.json";
 
-    private final String GenerateReportCommand = "cmd /c allure generate target/allure-results -o target/allure-report --clean";
+    private final String GenerateReportCommand = "cmd /c C:\\Users\\ahesh\\.jdks\\openjdk-25.0.1\\bin\\java.exe -classpath \"allure\\allure-2.36.0\\lib\\*;allure\\allure-2.36.0\\lib\\config\" io.qameta.allure.CommandLine generate target/allure-results -o target/allure-report --clean";
 
     public void onExecutionStart() {
         System.out.println("Execution started. Deleting old Allure results...");
